5154a30d0769ac111956a7a89f1e5518d6bcaa0b
aetos <aetos382@users.noreply.github.com>
2026-01-01 10:10:53 +0000

    add source generator and tests

---
+++ Analyzers/BlueBlaze.SourceGenerator/LexiconGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace BlueBlaze.SourceGenerator;
+
+public sealed class LexiconGenerator :
+    IIncrementalGenerator
+{
+    void IIncrementalGenerator.Initialize(
+        IncrementalGeneratorInitializationContext context)
+    {
+    }
+}
